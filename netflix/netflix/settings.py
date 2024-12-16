a1362f56cf29f99d13020211756848eac925096b
Nacho <isanccal@myuax.com>
2024-12-16 16:35:57 +0100

    postgree

--- netflix/netflix/settings.py
+++ netflix/netflix/settings.py
@@ -83,9 +83,13 @@ WSGI_APPLICATION = "netflix.wsgi.app"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': config("DB_ENGINE", default="django.db.backends.postgresql"),
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
     }
 }
 
